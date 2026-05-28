091c04ed326dae1980a54a18407ad53dabca46ff
rafidhp <rafiislamipasha27@gmail.com>
2026-05-29 03:37:20 +0700

    feat: add login and database conneection

--- src/khaslanainventaris/KhaslanaInventaris.java
+++ src/khaslanainventaris/KhaslanaInventaris.java
@@ -14,7 +14,7 @@ public class KhaslanaInventaris {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new LoginFrame().setVisible(true);
     }
     
 }
---
+++ src/khaslanainventaris/connection.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package khaslanainventaris;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author rafidhp
+ */
+public class connection {
+    Connection conn;
+    
+    public connection() {
+        String id, password, driver, url;
+        
+        id = "rafidhp";
+        password = "rafidhp_";
+        driver = "com.mysql.cj.jdbc.Driver";
+        url = "jdbc:mysql://localhost:3306/khaslana_inventaris?userTimezone=true&server=UTC";
+        
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url, id, password);
+            
+            if (conn == null) {
+                System.out.println("Koneksi Gagal");
+            } else {
+                System.out.println("Koneksi Berhasil");
+            }
+        } catch (Exception e) {
+            System.out.println("error database connection: " + e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+        connection dbc = new connection();
+    }
+}
